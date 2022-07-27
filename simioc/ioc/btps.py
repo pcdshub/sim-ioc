0493367bb4de6d75e602ae8f862d7f4e7a98e805
Ken Lauer <klauer@slac.stanford.edu>
2022-07-27 16:21:29 -0700

    MNT: adjust linear positions based on flange spacing

--- simioc/ioc/btps.py
+++ simioc/ioc/btps.py
@@ -23,62 +23,113 @@ class SourceDestinationConfiguration:
     goniometer: float
 
 
+# Bottom destinations (rough top centers, inside chamber)
+BOTTOM_PORTS = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+]
+TOP_PORTS = [
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+]
+
+
+def guess_position_for_port(dest: int) -> float:
+    """
+    Make a guess at a linear stage position given the destination port number.
+
+    Parameters
+    ----------
+    dest : int
+        The destination port number.
+
+    Returns
+    -------
+    float
+        Linear stage position guess.
+    """
+    # 8.5" spacing per side
+    # 215.9 mm port-to-port
+    port_spacing_mm = 215.9
+    top_start = 30.0000
+    bottom_start = top_start + port_spacing_mm / 2
+
+    if dest in TOP_PORTS:
+        port_index = TOP_PORTS.index(dest)
+        start = top_start
+    else:
+        port_index = BOTTOM_PORTS.index(dest)
+        start = bottom_start
+
+    return start + port_index * port_spacing_mm
+
+
 sample_config = sum(
     (
         [
-            # TMO IP1
+            # TMO IP1 (LD8)
             SourceDestinationConfiguration(
                 source=source,
-                destination=1,
-                linear=1357.0,
+                destination=8,
+                linear=guess_position_for_port(8),
                 rotary=-89.7,
                 goniometer=0.0,
             ),
-            # TMO IP2
+            # TMO IP2 (LD10)
+            SourceDestinationConfiguration(
+                source=source,
+                destination=10,
+                linear=guess_position_for_port(10),
+                rotary=-89.0,
+                goniometer=0.0,
+            ),
+            # TMO IP3 (LD2)
             SourceDestinationConfiguration(
                 source=source,
                 destination=2,
-                linear=1000.,
+                linear=guess_position_for_port(2),
                 rotary=-89.0,
                 goniometer=0.0,
             ),
-            # TMO IP3
-            SourceDestinationConfiguration(
-                source=source,
-                destination=3,
-                linear=750.,
-                rotary=-89.0,
-                goniometer=0.0,
-            ),
-            # qRIXS
-            SourceDestinationConfiguration(
-                source=source,
-                destination=4,
-                linear=500.,
-                rotary=-89.0,
-                goniometer=0.0,
-            ),
-            # ChemRIXS
-            SourceDestinationConfiguration(
-                source=source,
-                destination=5,
-                linear=250.,
-                rotary=-89.0,
-                goniometer=0.0,
-            ),
-            # XPP
+            # qRIXS (LD6)
             SourceDestinationConfiguration(
                 source=source,
                 destination=6,
-                linear=59.0,
+                linear=guess_position_for_port(6),
                 rotary=-89.0,
                 goniometer=0.0,
             ),
-            # Laser lab
+            # ChemRIXS (LD4)
             SourceDestinationConfiguration(
                 source=source,
-                destination=7,
-                linear=0.,
+                destination=4,
+                linear=guess_position_for_port(4),
+                rotary=-89.0,
+                goniometer=0.0,
+            ),
+            # XPP (LD14)
+            SourceDestinationConfiguration(
+                source=source,
+                destination=14,
+                linear=guess_position_for_port(14),
+                rotary=-89.0,
+                goniometer=0.0,
+            ),
+            # Laser lab (LD9)
+            SourceDestinationConfiguration(
+                source=source,
+                destination=9,
+                linear=guess_position_for_port(9),
                 rotary=-89.0,
                 goniometer=0.0,
             ),
